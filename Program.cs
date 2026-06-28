63bde061e776a14e7f6e031fe83f2eedfa5c7afd
Vidyullata Chavan <r.vidyu.s@gmail.com>
2026-06-28 18:18:45 +0530

    Add CORS policy for localhost:4200 support

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ namespace BookAPI
     {
       var builder = WebApplication.CreateBuilder(args);
       builder.Services.AddControllers();
+      builder.Services.AddCors(options =>
+      {
+        options.AddPolicy("MyCors", builder =>
+        {
+          builder.WithOrigins("http://localhost:4200/")
+                 .AllowAnyMethod()
+                 .AllowAnyHeader();
+        });
+      });
+
       var app = builder.Build();
+      app.UseCors("MyCors");
       app.MapControllers();
       //app.MapGet("/", () => "Hello World!");
-
       app.MapGet("/", () =>
       {
         return Results.Redirect("/api/books");
