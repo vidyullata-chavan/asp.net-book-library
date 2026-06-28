8bf63498b42a33cb95918478d3f75815342e8b03
Vidyullata Chavan <r.vidyu.s@gmail.com>
2026-06-28 17:10:07 +0530

    Add Books API with controller and model

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class BooksController : ControllerBase
+  {
+    private readonly List<Book> _books = new List<Book>
+    {
+      new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald" },
+      new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee" },
+      new Book { Id = 3, Title = "1984", Author = "George Orwell" }
+    };
+
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Book>> GetBooks()
+    {
+      return Ok(_books);
+    }
+
+  }
+}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,15 @@ namespace BookAPI
     public static void Main(string[] args)
     {
       var builder = WebApplication.CreateBuilder(args);
+      builder.Services.AddControllers();
       var app = builder.Build();
+      app.MapControllers();
+      //app.MapGet("/", () => "Hello World!");
 
-      app.MapGet("/", () => "Hello World!");
-
+      app.MapGet("/", () =>
+      {
+        return Results.Redirect("/api/books");
+      });
       app.Run();
     }
   }
